17de4d571106bda98efd68d39c6e862a472a4f84
kwk Cecilia .C <cc.comar36@gmail.com>
2025-07-23 14:51:46 -0400

    Creating first ineratctive UI

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "I'd love to know your name!"
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text(textTitle)
+                Text("-this is my interactive UI-")
+                TextField("Type your name here scholar!", text: $name)
+                Button("Continue") {
+                    textTitle =
+                    "Welcome \(name)!"
+                    name = ""
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
